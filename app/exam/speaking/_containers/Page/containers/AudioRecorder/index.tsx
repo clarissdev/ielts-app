aec539e6a4b019ecdec3d89f8ab2bd6f67e85a31
Thanh Dinh <dinhvanthanh0202@gmail.com>
2024-01-16 23:26:20 +0700

    feat: audio recorder viewer

--- app/exam/speaking/_containers/Page/containers/AudioRecorder/index.tsx
+++ app/exam/speaking/_containers/Page/containers/AudioRecorder/index.tsx
@@ -13,16 +13,67 @@ type Props = {
 
 export default function AudioRecorder({ className, style, onStop }: Props) {
   const [permission, setPermission] = React.useState(false);
-
   const mediaRecorder = React.useRef<MediaRecorder | null>(null);
-
   const [recordingStatus, setRecordingStatus] = React.useState("inactive");
-
   const [stream, setStream] = React.useState<MediaStream | null>(null);
-
   const [audioChunks, setAudioChunks] = React.useState<Blob[]>([]);
-
   const [notificationApi, notificationContextHolder] = useNotification();
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasCtx = canvasRef.current?.getContext("2d");
+
+  const visualize = (stream: MediaStream) => {
+    const audioCtx = new AudioContext();
+
+    const source = audioCtx.createMediaStreamSource(stream);
+
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    source.connect(analyser);
+
+    const draw = () => {
+      const canvas = canvasRef.current;
+      if (!canvas || !canvasCtx) return;
+
+      const WIDTH = canvas.width;
+      const HEIGHT = canvas.height;
+
+      requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = (WIDTH * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = (v * HEIGHT) / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+    };
+
+    draw();
+  };
 
   const getMicrophonePermission = async () => {
     if ("MediaRecorder" in window) {
@@ -49,6 +100,7 @@ export default function AudioRecorder({ className, style, onStop }: Props) {
     if (!stream) return;
     setRecordingStatus("recording");
     const media = new MediaRecorder(stream);
+    visualize(stream);
 
     mediaRecorder.current = media;
 
@@ -89,6 +141,12 @@ export default function AudioRecorder({ className, style, onStop }: Props) {
       {recordingStatus === "recording" ? (
         <Button onClick={stopRecording}>Stop Recording</Button>
       ) : null}
+      <div style={{ marginTop: "20px" }}>
+        <canvas
+          ref={canvasRef}
+          style={{ height: "60px", width: "300px" }}
+        ></canvas>
+      </div>
     </div>
   );
 }
