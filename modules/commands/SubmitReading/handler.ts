3fb4b62c405076d640d5cf5874f418bd9c8624cc
Thanh Dinh <110231356+thanh-kreate@users.noreply.github.com>
2024-01-19 14:05:50 +0700

    feat: redirect to /test (#4)
    * feat: redirect to /test
    
    * remove redundant function

--- modules/commands/SubmitReading/handler.ts
+++ modules/commands/SubmitReading/handler.ts
@@ -1,6 +1,11 @@
 import { Db } from "mongodb";
 
+import { getScoreFromNumCorrectQuestions } from "../../../app/submission/reading/[submissionId]/utils";
+import { handler$GetAnswerReading } from "../GetAnswerReading/handler";
+
 import { SubmitReading$Params, SubmitReading$Result } from "./typing";
+
+import { getQuestionId } from "@/modules/common-utils";
 
 type Options = {
   params: SubmitReading$Params;
@@ -11,9 +16,20 @@ export async function handler$SubmitReading(
   db: Db,
   { params, createdBy }: Options
 ) {
+  const answer = await handler$GetAnswerReading(db, {
+    examId: params.examId
+  });
+
+  const numCorrectAnswers = params.answer.filter((item, index) =>
+    answer.answers[getQuestionId(index + 1)].includes(item)
+  ).length;
+
+  const grade = getScoreFromNumCorrectQuestions(numCorrectAnswers);
+
   const { insertedId } = await db.collection("submission-reading").insertOne({
     createdBy,
     createdAt: Date.now(),
+    grade,
     ...params
   });
   return SubmitReading$Result.parse({ submissionId: insertedId.toHexString() });
