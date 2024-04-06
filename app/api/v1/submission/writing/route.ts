a135a88052bc95e091299912d94c52f6fea19976
Thanh Dinh <dinhvanthanh0202@gmail.com>
2024-04-06 23:55:48 +0700

    fix: minor

--- app/api/v1/submission/writing/route.ts
+++ app/api/v1/submission/writing/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { handler$GetSubmissionWritingList } from "@/modules/commands/GetSubmissionWritingList/handler";
-import { GetSubmissionWritingList$Params } from "@/modules/commands/GetSubmissionWritingList/typing";
+import { handler$GetSubmissionWriting } from "@/modules/commands/GetSubmissionWriting/handler";
+import { GetSubmissionWriting$Params } from "@/modules/commands/GetSubmissionWriting/typing";
 import { getDb } from "@/modules/mongodb";
 
 export async function GET(request: NextRequest) {
   const db = await getDb();
   const searchParams = Object.fromEntries(request.nextUrl.searchParams);
-  const params = GetSubmissionWritingList$Params.parse(searchParams);
-  const result = await handler$GetSubmissionWritingList(db, params);
+  const params = GetSubmissionWriting$Params.parse(searchParams);
+  const result = await handler$GetSubmissionWriting(db, params);
   return NextResponse.json(result);
 }
